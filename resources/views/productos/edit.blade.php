3f96cdb7d8b12cbc3a4800fc8c1753125c3969b5
unknown <al05-027-0319@utdelacosta.edu.mx>
2024-11-26 21:12:57 -0600

    agregar categoria

---
+++ resources/views/productos/edit.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+    <label for="descripcion">Descripción</label>
+    <textarea name="descripcion" id="descripcion" class="form-control" rows="3">{{ old('descripcion', $producto->descripcion) }}</textarea>
+</div>
